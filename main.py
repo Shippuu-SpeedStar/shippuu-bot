effba95d089f245fd6a653cacd8ee1fe7091e3d9
Shippuu-SpeedStar <tamgamecreator@gmail.com>
2025-03-06 16:31:49 +0900

    Update main.py

--- main.py
+++ main.py
@@ -33,7 +33,7 @@ async def on_message(message):
     user_count = sum(1 for member in guild.members if not member.bot)
     # BOTのみ
     bot_count = sum(1 for member in guild.members if member.bot)
-    await interaction.response.send(f'メンバー数：{member_count}ユーザー数：{user_count}ボット数：{bot_count}')
+    await message.response.send_message(f'メンバー数：{member_count}ユーザー数：{user_count}ボット数：{bot_count}')
 
 
 TOKEN = os.getenv("DISCORD_TOKEN")
