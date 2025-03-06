a944d329eac4ddf81805742716c1e060dd39fcfe
Shippuu-SpeedStar <tamgamecreator@gmail.com>
2025-03-07 01:11:23 +0900

    Update main.py

--- main.py
+++ main.py
@@ -30,16 +30,16 @@ async def on_message(message):
 
 @client.event
 async def on_message(call_message):
-    if message.author != client.user:
+    if call_message.author != client.user:
         if client.user in call_message.mentions: # 話しかけられたかの判定
             reply = f'{call_message.author.mention} 呼びましたか？' # 返信メッセージの作成
             await call_message.channel.send(reply) # 返信メッセージを送信
         
-#@client.event
-#async def on_message(hello_message):
-#    if message.author != client.user:
-#        if hello_message.content == "こんにちは！": # 話しかけられたかの判定
-#            await hello_message.channel.send('こんにちは！') # 返信メッセージを送信
+@client.event
+async def on_message(hello_message):
+    if hello_message.author != client.user:
+        if hello_message.content == "こんにちは！": # 話しかけられたかの判定
+            await hello_message.channel.send('こんにちは！') # 返信メッセージを送信
 
 
 TOKEN = os.getenv("DISCORD_TOKEN")
