732899de585cf7f90b8f3fb5199d36fb32cdc28e
Shippuu-SpeedStar <tamgamecreator@gmail.com>
2025-03-06 14:21:30 +0900

    Update main.py

--- main.py
+++ main.py
@@ -7,11 +7,20 @@ client = discord.Client(intents=discord.Intents.default())
 @client.event
 async def on_ready():
     print('ログインしました')
+ # アクティビティを設定
+    new_activity = f"テスト"
+    await client.change_presence(activity=discord.Game(new_activity))
 
+    # スラッシュコマンドを同期
+    await tree.sync()
 # @client.event
 # async def on_message(message):
  #   emoji ="👍"
   #  await message.add_reaction(emoji)
+@tree.command(name='hello', description='Say hello to the world!') 
+async def test(interaction: discord.Interaction): 
+  await interaction.response.send_message('こんにちは！')
+
 
 TOKEN = os.getenv("DISCORD_TOKEN")
 # Web サーバの立ち上げ
