6993261c3791ba80245e0e41cd89557a4085b4c0
Shippuu-SpeedStar <tamgamecreator@gmail.com>
2025-03-07 21:41:59 +0900

    Update main.py

--- main.py
+++ main.py
@@ -49,8 +49,8 @@ async def on_message(message):
         else:
             await message.channel.send('VCに接続してから言ってください')
     elif message.content == "疾風、VC退出です！":
-        #global voiceChannel
-        await message.voice_client.disconnect()
+        voiceChannel.stop()
+        await voiceChannel.disconnect()
         #await message.author.voice.channel.leave()
         await message.channel.send("ありがとうございました！")
 
