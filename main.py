597c904ec7224b1f94e3c81df425094d86b4f369
Shippuu-SpeedStar <tamgamecreator@gmail.com>
2025-03-07 21:34:50 +0900

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import os
 from keep_alive import keep_alive
 from discord import app_commands
 import asyncio
+from discord.channel import VoiceChannel
 
 intents = discord.Intents.all()
 intents.message_content = True
