d0b4b7de688cd779d6579fe563ec968db8681b84
Shippuu-SpeedStar <tamgamecreator@gmail.com>
2026-09-09 23:15:20 +0900

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from discord import app_commands
 from discord.ext import commands, tasks
 import weather
 import BombGame
-import DanmakuGameView
+import Danmaku
 import topic
 import re
 import asyncio
